b503b194ece9a5b848974b5214371292edb96e24
Maxthegreat99 <Kerlan.jacquette@gmail.com>
2023-10-28 18:57:37 -0400

    Create XmlDefaultConfigs
    Made a class devs can extend to easily add comments to their configs.

---
+++ _Utility Classes/XmlDefaultConfigs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Terraria.Plugins.Common._Utility_Classes
+{
+    /// <summary>
+    /// Allows for the use of [XmlComment(value = "Comments")] when
+    /// on properties extending this class
+    /// </summary>
+    public class XmlDefaultConfigs : IXmlSerializable
+    {
+
+        public void WriteXml(XmlWriter writer)
+        {
+            var properties = GetType().GetProperties();
+
+            foreach (var propertyInfo in properties)
+            {
+                if (propertyInfo.IsDefined(typeof(XmlCommentAttribute), false))
+                {
+                    writer.WriteComment(
+                        propertyInfo.GetCustomAttributes(typeof(XmlCommentAttribute), false)
+                            .Cast<XmlCommentAttribute>().Single().Value);
+                }
+
+                writer.WriteElementString(propertyInfo.Name, propertyInfo.GetValue(this, null).ToString());
+            }
+        }
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class XmlCommentAttribute : Attribute
+    {
+        public string Value { get; set; }
+    }
+}
